1e5cce31488f3e9f458d2615438ea240e5671d47
Onur Kerimov <onurkerimov@gmail.com>
2022-06-05 19:21:19 +0300

    Remove dist

--- dist/devtools/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Atom } from '@xoid/engine';
-
-/**
- * Integration for [Redux DevTools Extension](https://github.com/zalmoxisus/redux-devtools-extension).
- */
-declare const devtools: <T extends unknown>(atom: Atom<T>, name?: string | undefined) => () => void;
-
-export { devtools };
