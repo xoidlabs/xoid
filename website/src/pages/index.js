b96b820a3c728ae68857f6a65c660fb4596ea350
Onur Kerimov <onurkerimov@gmail.com>
2022-06-08 14:14:55 +0300

    Update styles

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -284,17 +284,16 @@ const Index = () => {
       ))}
       <Section background="none" className="Embed" style={{ display: 'flex', justifyContent: 'center', padding: 25 }}>
         <iframe
-          src="https://codesandbox.io/embed/competent-carson-j2tfqm?fontsize=12&hidenavigation=1&theme=dark"
+          src="https://codesandbox.io/embed/cool-dream-wp9rn6?fontsize=14&hidenavigation=1&theme=dark"
           style={{
             width: "100%",
             maxWidth: 1080,
-            height: 500,
+            height: 400,
             border: 0,
             borderRadius: 4,
             overflow: "hidden"
           }}
           title="competent-carson-j2tfqm"
-          allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
           sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
         />
       </Section>
