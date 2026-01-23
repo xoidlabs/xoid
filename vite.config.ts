9e52cea5e433fbcc70570c0f1f7033630d268cfb
Onur Kerimov <onurkerimov@gmail.com>
2026-01-24 00:50:21 +0100

    Add description

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 // import macrosPlugin from "vite-plugin-babel-macros"
 
+// This only serves for manual testing of the library
 export default defineConfig({
   root: './tests/manual-testing/src',
   plugins: [
